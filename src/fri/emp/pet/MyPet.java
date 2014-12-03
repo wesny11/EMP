11f75e29aa0784ff2a0a8fd6db017a1b2a965607
tomzig <gorencT123>
2014-12-03 11:10:44 +0100

    Commit

---
+++ src/fri/emp/pet/MyPet.java
@@ -0,0 +1,15 @@
+package fri.emp.pet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyPet extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/fri/emp/pet/ViewPets.java
@@ -0,0 +1,15 @@
+package fri.emp.pet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tomaz on 29.11.2014.
+ */
+public class ViewPets extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_pets);
+    }
+}
